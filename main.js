e837a0baf4c57c9777fe15c8d84f3081bae1c835
Jordy <avilajordy71@gmail.com>
2024-03-04 20:25:59 -0500

    conected the menu and shopping cart

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-const email = document.querySelector(".navbar-email")
-const menuDesktop = document.querySelector('.desktop-menu')
+const navEmail = document.querySelector(".navbar-email");
+const menuDesktop = document.querySelector('.desktop-menu');
+const menuHamburguer = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuCarrito = document.querySelector('.product-details')
 
-email.addEventListener("click", toogleDesktopMenu)
+navEmail.addEventListener("click", toogleDesktopMenu);
+menuHamburguer.addEventListener('click', toogleMobileMenu )
+menuCarritoIcon.addEventListener('click', toogleCarritoMenu )
 
 function toogleDesktopMenu(){
-    menuDesktop.classList.toggle('inactive')
+    menuDesktop.classList.toggle('inactive');
 }
+function toogleMobileMenu(){
+    menuMobile.classList.toggle('inactive');
+}
+function toogleCarritoMenu(){
+    menuCarrito.classList.toggle('inactive')
+}
+
